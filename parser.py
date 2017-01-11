facc953a42c561997c7f45867e6b2f16fdb247b1
Christian <quintavalle.christian@gmail.com>
2017-01-11 18:21:11 +0100

    Change meh chris

--- parser.py
+++ parser.py
@@ -1,4 +1,4 @@
-#[superChangesRicky]Must be controlled before use
+#[change della minchia di christian]Must be controlled before use
 
 from __future__ import division
 import xml.etree.cElementTree as ElementTree
