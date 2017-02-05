414b20294b62313162fdbac0dc2eeda30fd8413b
Christian <quintavalle.christian@gmail.com>
2017-02-05 22:17:09 +0100

    Update api for offline version

--- Client/play/js/services/api.js
+++ Client/play/js/services/api.js
@@ -2,54 +2,55 @@ angular.module('play')
 .factory('Api', function ApiFactory($http){
 	user_id=2;
 	var u,us,f,r,l,b,m,mp,pp,ms,t;
+	var BASE_URL = "localhost/server"
 	return{
 		user:		function(){
 						//if(typeof u === "undefined"){
-							u = $http({method: 'GET', url: 'http://127.0.0.1:8000/server/users/'+user_id+'/?include=user_stats'});
+							u = $http({method: 'GET', url: BASE_URL+'/users/'+user_id+'/?include=user_stats'});
 						//}
 						return u;
 					},
 					
 		users:		function(){
-							us = $http({method: 'GET', url: 'http://127.0.0.1:8000/server/users/'});
+							us = $http({method: 'GET', url: BASE_URL+'/users/'});
 						return us;
 					},
 		favourites:	function(){
-						f = $http({method: 'GET', url: 'http://127.0.0.1:8000/server/boardgames/favourites/?user_id='+user_id});
+						f = $http({method: 'GET', url: BASE_URL+'/boardgames/favourites/?user_id='+user_id});
 						return f;
 					},
 					
 		recents:	function(){
-						r = $http({method: 'GET', url: 'http://127.0.0.1:8000/server/boardgames/recents/?user_id='+user_id});
+						r = $http({method: 'GET', url: BASE_URL+'/boardgames/recents/?user_id='+user_id});
 						return r;
 					},
 		boadgames:		function(){
-						l = $http({method: 'GET', url: 'http://chrissj2.pythonanywhere.com/server/boardgames/?user_id='+user_id});
+						l = $http({method: 'GET', url: BASE_URL+'/boardgames/?user_id='+user_id});
 						return l;
 					},
 		boardgame:	function(id){
-						b = $http({method: 'GET', url: 'http://127.0.0.1:8000/server/boardgames/'+id+'/?include=matches&user_id='+user_id});
+						b = $http({method: 'GET', url: BASE_URL+'/boardgames/'+id+'/?include=matches&user_id='+user_id});
 						return b;
 					},		
 		match:		function(id){
-						m = $http({method: 'GET', url: 'http://127.0.0.1:8000/server/matches/'+id+'/?user_id='+user_id});
+						m = $http({method: 'GET', url: BASE_URL+'/matches/'+id+'/?user_id='+user_id});
 						return m;
 					},	
 		matches:	function(){
-						ms = $http({method: 'GET', url: 'http://127.0.0.1:8000/server/boardgames/recents/?user_id='+user_id+'&include=matches'});
+						ms = $http({method: 'GET', url: BASE_URL+'/boardgames/recents/?user_id='+user_id+'&include=matches'});
 						return ms;
 					},	
 		matchpost:	function(match){
-						mp = $http({method: 'POST', url: 'http://127.0.0.1:8000/server/matches/', data:match});
+						mp = $http({method: 'POST', url: BASE_URL+'/matches/', data:match});
 						return mp;
 					},
 					
 		playpost:	function(plays){
-						pp = $http({method: 'POST', url: 'http://127.0.0.1:8000/server/plays/', data:plays});
+						pp = $http({method: 'POST', url: BASE_URL+'/plays/', data:plays});
 						return pp;
 					},
 		template:	function(boardgame){
-						t = $http({method: 'GET', url: 'http://127.0.0.1:8000/server/templates/'+boardgame+'/'});
+						t = $http({method: 'GET', url: BASE_URL+'/templates/'+boardgame+'/'});
 						return t;
 					},	
 		
